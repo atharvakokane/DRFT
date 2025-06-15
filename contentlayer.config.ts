f4b83b3e9952921e6e9ca736dc963ee8f91a0d46
Atharva S. Kokane <atharvashashankkokane@gmail.com>
2025-06-15 17:36:05 +0400

    Create contentlayer.config.ts

---
+++ contentlayer.config.ts
@@ -0,0 +1,18 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+
+const Article = defineDocumentType(() => ({
+  name: 'Article',
+  filePathPattern: `**/*.md`,
+  contentType: 'markdown',
+  fields: {
+    title: { type: 'string', required: true },
+    description: { type: 'string', required: true },
+    date: { type: 'date', required: true },
+    slug: { type: 'string', required: true },
+  },
+}))
+
+export default makeSource({
+  contentDirPath: 'content',
+  documentTypes: [Article],
+})
